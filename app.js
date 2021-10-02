2d465f0e5dcfe4f40875be420cd9bfd8a3d40950
Pravek Tirupari <pravektirupari@gmail.com>
2021-10-02 23:43:43 +0530

    Initialize server with the home route

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+// if (process.env.NODE_ENV === "development") {
+//   morgan("dev");
+// }
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/public/signup.html`);
+});
+
+app.post("/", (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  res.send("Data submitted");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server up and running at port ${process.env.PORT}`);
+});
